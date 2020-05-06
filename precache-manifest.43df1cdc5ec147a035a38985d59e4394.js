afde770040afbc6c04ff6c69b6c98382571c959a
SarahBechi <59558883+SarahBechi@users.noreply.github.com>
2020-05-06 21:01:32 +0000

    Updates

---
+++ precache-manifest.43df1cdc5ec147a035a38985d59e4394.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93dd63e1a1b7dbc394b4412cd21e33cc",
+    "url": "/miss-confitures/index.html"
+  },
+  {
+    "revision": "2490c49a29f965acca89",
+    "url": "/miss-confitures/static/css/2.e048c457.chunk.css"
+  },
+  {
+    "revision": "c4b47a8eb97dee841eef",
+    "url": "/miss-confitures/static/css/main.cee6728f.chunk.css"
+  },
+  {
+    "revision": "2490c49a29f965acca89",
+    "url": "/miss-confitures/static/js/2.dec84eb2.chunk.js"
+  },
+  {
+    "revision": "f8fdf8f75592187089495adb43d53d96",
+    "url": "/miss-confitures/static/js/2.dec84eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b47a8eb97dee841eef",
+    "url": "/miss-confitures/static/js/main.867a9b0d.chunk.js"
+  },
+  {
+    "revision": "8d5bcbe3d50de9c72a69",
+    "url": "/miss-confitures/static/js/runtime-main.3bb63425.js"
+  },
+  {
+    "revision": "4a94fc48b900bf3b074d6b3bacf8f139",
+    "url": "/miss-confitures/static/media/cover_half_res.4a94fc48.webp"
+  },
+  {
+    "revision": "3ccd5c6da5a342a87601fab1fceb7e07",
+    "url": "/miss-confitures/static/media/ecolo.3ccd5c6d.jpg"
+  },
+  {
+    "revision": "161ec45a610ebcc843c1ecca3e479e59",
+    "url": "/miss-confitures/static/media/email.161ec45a.png"
+  },
+  {
+    "revision": "5c54bdd5a226060b08a2a8c17e802b29",
+    "url": "/miss-confitures/static/media/engineer.5c54bdd5.png"
+  },
+  {
+    "revision": "37a8e9d07579222207fd379aa3ec531c",
+    "url": "/miss-confitures/static/media/healthyJam.37a8e9d0.png"
+  },
+  {
+    "revision": "7130413da851c934c7a461d1a62aecff",
+    "url": "/miss-confitures/static/media/ingredient.7130413d.png"
+  },
+  {
+    "revision": "bbaa9fbdb9fa0209ab23ca9d18360ea9",
+    "url": "/miss-confitures/static/media/jar.bbaa9fbd.png"
+  },
+  {
+    "revision": "89994fb07ede064e923e2f0fcb6865c9",
+    "url": "/miss-confitures/static/media/logo.89994fb0.png"
+  },
+  {
+    "revision": "03b166b829fbdb94e791c5d8e56788f6",
+    "url": "/miss-confitures/static/media/phone.03b166b8.png"
+  },
+  {
+    "revision": "bebd2eefe0925c2566d106f96607fe6b",
+    "url": "/miss-confitures/static/media/stevia.bebd2eef.png"
+  },
+  {
+    "revision": "6f49504bd97e650a44dc5b1e10ac5b66",
+    "url": "/miss-confitures/static/media/store.6f49504b.png"
+  },
+  {
+    "revision": "0fa82b5c995cef1eeb7e79ab875873de",
+    "url": "/miss-confitures/static/media/toteBag.0fa82b5c.png"
+  }
+]);
